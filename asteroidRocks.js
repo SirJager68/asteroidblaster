e93ca960b952b0f7d0160b26050421061e9e8894
jager <jagerbb@gmail.com>
2025-04-21 10:59:02 -0500

    Initial commit after fixing filename and identity

---
+++ asteroidRocks.js
@@ -0,0 +1,68 @@
+//ROCK 1
+ctx.beginPath();
+ctx.moveTo(0 * s, -10 * s);
+ctx.lineTo(-5 * s, -15 * s);
+ctx.lineTo(-10 * s, -10 * s);
+ctx.lineTo(-10 * s, 0 * s);
+ctx.lineTo(-5 * s, 5 * s);
+ctx.lineTo(0 * s, 5 * s);
+ctx.lineTo(8 * s, 0 * s);
+ctx.lineTo(5 * s, -5 * s);
+ctx.lineTo(8 * s, -10 * s);
+ctx.lineTo(5 * s, -15 * s);
+ctx.lineTo(0 * s, -10 * s);
+ctx.closePath();
+ctx.stroke();
+
+//ROCK 2
+ctx.beginPath();
+ctx.moveTo(0 * s, -12 * s);
+ctx.lineTo(-5 * s, -15 * s);
+ctx.lineTo(-10 * s, -10 * s);
+ctx.lineTo(-7 * s, -5 * s);
+ctx.lineTo(-10 * s, 0 * s);
+ctx.lineTo(-5 * s, 5 * s);
+ctx.lineTo(-3 * s, 3 * s);
+ctx.lineTo(5 * s, 5 * s);
+ctx.lineTo(10 * s, -3 * s);
+ctx.lineTo(5 * s, -7 * s);
+ctx.lineTo(10 * s, -10 * s);
+ctx.lineTo(5 * s, -15 * s);
+ctx.lineTo(0 * s, -12 * s);
+ctx.closePath();
+ctx.stroke();
+
+//ROCK 3
+ctx.beginPath();
+ctx.moveTo(-3 * s, -15 * s);
+ctx.lineTo(3 * s, -15 * s);
+ctx.lineTo(10 * s, -7 * s);
+ctx.lineTo(10 * s, -2 * s);
+ctx.lineTo(4 * s, 10 * s);
+ctx.lineTo(-1 * s, 10 * s);
+ctx.lineTo(-1 * s, 3 * s);
+ctx.lineTo(-5 * s, 10 * s);
+ctx.lineTo(-10 * s, 0 * s);
+ctx.lineTo(-5 * s, -2 * s);
+ctx.lineTo(-10 * s, -5 * s);
+ctx.lineTo(-3 * s, -15 * s);
+ctx.closePath();
+ctx.stroke();
+
+//ROCK 4
+ctx.beginPath();
+ctx.moveTo(-3 * s, -15 * s);
+ctx.lineTo(0 * s, -10 * s);
+ctx.lineTo(-10 * s, -10 * s);
+ctx.lineTo(-10 * s, -1 * s);
+ctx.lineTo(-5 * s, 8 * s);
+ctx.lineTo(5 * s, 5 * s);
+ctx.lineTo(8 * s, 8 * s);
+ctx.lineTo(13 * s, 3 * s);
+ctx.lineTo(5 * s, -3 * s);
+ctx.lineTo(13 * s, -6 * s);
+ctx.lineTo(13 * s, -10 * s);
+ctx.lineTo(5 * s, -15 * s);
+ctx.lineTo(-3 * s, -15 * s);
+ctx.closePath();
+ctx.stroke();
